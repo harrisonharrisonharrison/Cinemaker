24cfc4a12962dccf39afafb023a788dc4ec70a27
Harrison Tran <harrit4@uci.edu>
2025-04-20 00:43:27 -0700

    fixed ui

--- citrushacks/src/main.tsx
+++ citrushacks/src/main.tsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import TrailerPlayer from './Buttons.tsx'
+import Description from './Description.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='absolute top-0 left-0 -z-20 bg-radial-[at_50%_75%] from-black to-rose-950 min-h-screen w-full'/>
-
+    <div className='absolute top-0 left-0 -z-20 bg-radial-[at_50%_75%] from-black to-rose-950 min-h-screen h-[150vh] w-full'/>
+    
     <App/>
-     
-    {/*<TrailerPlayer />*/}
+    <Description />
 
   </StrictMode>,
 )
