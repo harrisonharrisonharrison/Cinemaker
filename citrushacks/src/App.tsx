8579a09ec277fe1df4c4f712814427e3c2e3ec57
Harrison Tran <harrit4@uci.edu>
2025-04-19 18:10:55 -0700

    added sliding curtains

---
+++ citrushacks/src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import curtain from './assets/curtain.png'
+
+
+function App() {
+  const [slideOut, setSlideOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSlideOut(true), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className='overflow-hidden'>
+    
+      <div className="">
+          <span className="absolute top-10 left-100 mx-auto py-4 flex bg-gradient-to-r blur-xl from-slate-50 bg-clip-text text-6xl font-extrabold select-none">
+            Welcome to Cinemaker
+          </span>
+          <h1
+              className="absolute top-10 left-110 w-fit h-auto py-4 justify-center flex bg-gradient-to-r items-center  from-slate-50 bg-clip-text text-6xl font-extrabold text-center select-auto">
+              Welcome to Cinemaker
+          </h1>
+      </div>
+
+      <div className='overflow-hidden'>
+      <div className={`absolute inset-0 z-20 transition-transform duration-[2000ms] ease-in-out ${
+            slideOut ? 'translate-x-full overflow-hidden' : 'translate-x-0'
+          }`}
+        >
+        <img src={curtain}
+        className="absolute top-0 left-1/2 w-full object-cover"/>
+      </div>
+      
+      <div className={`absolute inset-0 z-20 transition-transform duration-[2000ms] ease-in-out ${
+            slideOut ? '-translate-x-full overflow-hidden' : 'translate-x-0'
+          }`}
+        >
+        <img src={curtain}
+        className="absolute top-0 right-1/2 w-full object-cover"/>
+      </div>
+      </div>
+    
+    
+    </div>
+    
+  )
+}
+
+export default App
